a6bee22b7aa780fead33d2545eb0f88ae4c13bf0
Biisal <bisal.int@gmail.com>
2024-09-26 23:33:47 +0530

    forced dark theme

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "next-themes";
 
 export const ThemeWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider defaultTheme="dark" forcedTheme="light">
+    <ThemeProvider defaultTheme="dark" forcedTheme="dark">
       {children}
     </ThemeProvider>
   );
