80b176dd6b0ad044be6305537719381e2bb932a1
Thai Duy <thdangduy@gmail.com>
2026-05-10 19:35:27 +0700

    removed unneed files

--- api.ts
+++ api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = (process.env.NEXT_PUBLIC_SITE_URL || "https://biisal.codeltix.com") + "/api";
+// const BASE_URL = (process.env.NEXT_PUBLIC_SITE_URL || "https://portfolio.thaiduy.digital") + "/api";
 const BASE_URL = "/api";
 
 const api = axios.create({
